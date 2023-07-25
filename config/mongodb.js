a9ef65aec0c8b59d8179277ebb0c9d6c869b499b
CHANCHAL TYAGI <tyagichanchal407@gmail.com>
2023-07-25 22:46:18 +0530

    finished

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb+srv://tyagichanchal407:Shahenshah@cluster0.vmyxmud.mongodb.net/?retryWrites=true&w=majority').then(
+mongoose.connect('mongodb+srv://tyagichanchal407:Shahenshah@cluster0.vmyxmud.mongodb.net/employee_review_system?retryWrites=true&w=majority').then(
     () => {console.log("Database connected successfully")},
     (err) => {console.log("error occuring while database connection");}
 );
